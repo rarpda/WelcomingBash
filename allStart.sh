52edcce9bc4d6a78ef0bba87d8440127d55c121c
rarpda <ricardoarpauk@gmail.com>
2021-09-01 15:37:13 +0100

    Added ' to regex whitelist.

--- allStart.sh
+++ allStart.sh
@@ -3,11 +3,11 @@ path=$(($RANDOM % 2))
 
 if [[ path -eq 1 ]]; then
 output=$(curl -s -X GET "https://api.adviceslip.com/advice" | jq .slip.advice)
-CLEAN=${output//[^a-zA-z0-9 ,.?!]/}
+CLEAN=${output//[^a-zA-z0-9 ,.?!']/}
 echo "Random tip: $CLEAN"
 else
 output=$(curl -s  -X GET 'https://api.tronalddump.io/random/quote' -H "Accepts:application/json" | jq .value)
-CLEAN=${output//[^a-zA-z0-9 ,.?!]/}
+CLEAN=${output//[^a-zA-z0-9 ,.?!']/}
 echo "Donald trump once said: $CLEAN"
 fi
 
